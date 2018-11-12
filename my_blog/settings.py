42605168d2ac3741123c4250d4623a4ece53da84
liverphong <tf101918@126.com>
2018-11-12 22:10:16 +0800

    bushu

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -92,6 +92,10 @@ DATABASES = {
         'PASSWORD': '101918',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -133,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
